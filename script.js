9dd70ec4f38c144bff1cbf948db9c88fd1f0cd5e
shchyar <67362174+shchyar@users.noreply.github.com>
2020-10-17 20:54:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const insert = (num) => {
-  document.form.textview.value = document.form.textview.value + num;
+
+    document.form.textview.value = document.form.textview.value + num;
+
 };
 
 const result = () => {
-   let res = document.form.textview.value;
-  if(res) {
-     document.form.textview.value = eval(res);
+
+    let res = document.form.textview.value;
+      if(res) {
+        document.form.textview.value = eval(res);
   };
+
 };
 
- const clearResult = () => {
-  document.form.textview.value = "";
+const clearResult = () => {
+
+    document.form.textview.value = "";
+
+ }
+
+const arrow = () => {
+
+    let res = document.form.textview.value;
+    document.form.textview.value = res.substring(0,res.length-1);
+    
  }
