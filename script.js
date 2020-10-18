418cb93ce08abcb1753fe8ab0364e766591166a9
shchyar <67362174+shchyar@users.noreply.github.com>
2020-10-18 19:51:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let journal = [];
+let div = document.createElement('div');
+div.classList.add('hidden');
+document.body.append(div);
+let hidCons = document.getElementById('console');
+div.hidden = true;
+hidCons.hidden = false;
+
+
+
+const journalArr = () => {
+
+    div.innerHTML = (journal.join('<br>'));
+      if(div.hidden == true && hidCons.hidden == false){
+          div.hidden = false;
+          hidCons.hidden = true;
+      } 
+      else{
+        div.hidden = true;
+        hidCons.hidden = false;
+      }
+  
+}
+
 const insert = (num) => {
 
     document.form.textview.value = document.form.textview.value + num;
@@ -5,10 +29,12 @@ const insert = (num) => {
 };
 
 const result = () => {
-
+  
     let res = document.form.textview.value;
-      if(res) {
-        document.form.textview.value = eval(res);
+        if(res) {
+            document.form.textview.value = eval(res);
+            journal.push(`${res} =  ${document.form.textview.value}`)
+          
   };
 
 };
@@ -25,3 +51,4 @@ const arrow = () => {
     document.form.textview.value = res.substring(0,res.length-1);
     
  }
+
