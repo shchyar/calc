32d283bc76e497ac6d3c704c3c7092da5efaa42e
shchyar <67362174+shchyar@users.noreply.github.com>
2020-10-31 00:42:02 +0300

    сделал степень, корень, привязал к кнопкам, память журнала

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ let hidJournal = document.getElementById('hidden');
 hidCons.hidden = false;
 hidJournal.hidden = true;
 let controlCount = 0;
+let functionIndication = false;
+let res;
+
 let sesStor = sessionStorage.getItem('memory');
 
+
 if (journal == 0 && sesStor !== null) {
-    journal = sesStor.split('  ');
+    journal = sesStor.split(' ,');
+    console.log(journal);
     hidJournal.innerHTML = (journal.join('<br>'));
 }
 
@@ -40,13 +45,18 @@ document.addEventListener("keydown", function(event) {
             journalArr();
             controlCount = 0;
         };
+    } else if (event.key == "P" || event.key == "p") {
+        combine('pow');
+        light();
+    } else if (event.key == "S" || event.key == "s") {
+        combine('sqrt');
+        light();
     } else {
         document.focus;
     }
 });
 
 const journalArr = () => {
-
 
     if (hidJournal.hidden == true && hidCons.hidden == false) {
         hidJournal.hidden = false;
@@ -60,61 +70,78 @@ const journalArr = () => {
 
 const insert = (num) => {
 
-    document.form.textview.value = document.form.textview.value + num;
-
+    let input = document.querySelector("input");
+    input.setRangeText(num, input.selectionEnd, input.selectionStart, 'end');
+    input.focus();
 };
 
-const power = () => {
-    //document.form.textview.value = ('POWER');
+const combine = (value) => {
+
     let res = document.form.textview.value;
-    document.form.textview.value = (res+ ' '  + '** ' );
+    try {
+        let rEval = eval(res);
+    } catch (e) {
+        document.form.textview.value = ('ERROR');
+        setTimeout(() => document.form.textview.value = "", 500);
+    }
+    if (value == 'pow') {
+        document.form.textview.value = (value + '(' + eval(res) + ',' + '' + ')');
+    } else if (value == 'sqrt') {
+        document.form.textview.value = (value + '(' + eval(res) + ')');
+    }
     let input = document.querySelector("input");
     input.focus();
-    input.selectionStart = input.value.length;
-};
+    input.selectionStart = [input.value.length - 1];
+    input.selectionEnd = [input.value.length - 1];
+    functionIndication = true;
 
-const sqrt = () => {
-    document.form.textview.value = ('SQRT');
 };
 
 const result = () => {
 
+    if (functionIndication == true) {
+        res = document.form.textview.value;
+        let powResult = 'Math.' + res;
+        document.form.textview.value = (eval(powResult));
+        functionIndication = false;
+    } else {
+        res = document.form.textview.value;
+        if (res) {
+            try {
+                document.form.textview.value = eval(res);
+            } catch (e) {
+
+                document.form.textview.value = ('ERROR');
+                setTimeout(() => document.form.textview.value = "", 500);
+            };
+        };
+
+    };
+    writeJournal();
+};
+
+const writeJournal = () => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    let res = document.form.textview.value;
-    if (res) {
-        try {
-            document.form.textview.value = eval(res);
-        } catch (e) {
+    journal.unshift(`(${hours} : ${minutes} : ${seconds}) ${res} = ${document.form.textview.value}  `);
+    hidJournal.innerHTML = (journal.join('<br>'));
+    sessionStorage.setItem('memory', journal);
 
-            document.form.textview.value = ('ERROR');
-            setTimeout(() => document.form.textview.value = "", 500);
-        };
-        journal.unshift(`(${hours} : ${minutes} : ${seconds}) ${res} = ${document.form.textview.value}  `);
-        hidJournal.innerHTML = (journal.join('<br>'));
-        let value = hidJournal.outerText;
-        sessionStorage.setItem('memory', value);
-
-        if (journal.length > 9) {
-            journal.pop();
-        };
+    if (journal.length > 9) {
+        journal.pop();
     };
 
-};
+}
 
 const clearResult = () => {
-
     document.form.textview.value = "";
-
 };
 
 const arrow = () => {
-
     let res = document.form.textview.value;
     document.form.textview.value = res.substring(0, res.length - 1);
-
 };
 
 const checkBtn = (key) => {
